58bbf25b1a915e2d62a69ae9844e3ba20f3caade
Yahya Önder <06yahya06@gmail.com>
2022-05-29 23:45:36 +0300

    Add project files.

---
+++ EFcore_DBfirst_Blog_WindowsForm/Entities/Konular.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFcore_DBfirst_Blog_WindowsForm.Entities
+{
+    public partial class Konular
+    {
+        public Konular()
+        {
+            Yorumlars = new HashSet<Yorumlar>();
+        }
+
+        public int Id { get; set; }
+        public string Baslik { get; set; } = null!;
+        public string? Aciklama { get; set; }
+        public DateTime Tarih { get; set; }
+
+        public virtual ICollection<Yorumlar> Yorumlars { get; set; }
+    }
+}
---
+++ EFcore_DBfirst_Blog_WindowsForm/Form1.cs
@@ -0,0 +1,10 @@
+namespace EFcore_DBfirst_Blog_WindowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
